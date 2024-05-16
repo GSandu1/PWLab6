933cf824eb89743a289aad794254a907a4ac8223
GSandu1 <sandu.gazea@mail.ru>
2024-05-16 20:38:03 +0300

    drawer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <div id="app" class="container">
-    <label class="switch">
-      <input type="checkbox" @change="toggleTheme" :checked="darkTheme">
-      <span class="slider round"></span>
-    </label> 
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/likedvideos">Liked Videos</router-link>
-    </nav>
+    <button @click="drawerOpen = !drawerOpen" class="drawer-toggle">Toggle Drawer</button>
+    <Drawer :is-open="drawerOpen" :is-dark-theme="darkTheme" 
+            @toggle-drawer="drawerOpen = !drawerOpen"
+            @toggle-theme="toggleTheme" />
     <router-view :videos="videos" :selectVideo="selectVideo" :handleUnlike="handleUnlike" 
                  @videoSelect="onVideoSelect" @termChange="fetchVideos">
     </router-view>
@@ -16,14 +12,20 @@
 
 <script>
 import axios from "axios";
+import Drawer from './components/Drawer.vue';
 
-const API_KEY = 'AIzaSyCYKsZPI3YLGukHupLE2Mazc4tGbnxgp80';
+
+const API_KEY = '';
 const YOUTUBE_V3_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 export default {
   name: "App",
+  components: {
+    Drawer
+  },
   data() {
     return {
+      drawerOpen: false,
       videos: [],
       selectVideo: null,
       darkTheme: false
@@ -33,6 +35,7 @@ export default {
     toggleTheme() {
       this.darkTheme = !this.darkTheme;
       document.body.className = this.darkTheme ? "dark-theme" : "";
+      
     },
     fetchVideos(searchTerm) {
       axios.get(YOUTUBE_V3_URL, {
@@ -87,61 +90,5 @@ input {
   color: var(--input-text);
 }
 
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-}
-
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  transition: .4s;
-  border-radius: 34px;
-}
-
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  transition: .4s;
-  border-radius: 50%;
-}
-
-input:checked + .slider {
-  background-color: #2196F3;
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px #2196F3;
-}
-
-input:checked + .slider:before {
-  transform: translateX(26px);
-}
-
-.slider.round {
-  border-radius: 34px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
-}
 
 </style>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,111 @@
+<template>
+    <div :class="['drawer', { 'is-open': isOpen }]">
+      <button @click="toggleDrawer">Toggle Drawer</button>
+      <div class="drawer-content">
+        <nav>
+          <router-link to="/">Home</router-link>
+          <router-link to="/likedvideos">Liked Videos</router-link>
+        </nav>
+        <label class="switch">
+          <input type="checkbox" @change="$emit('toggle-theme')" :checked="isDarkTheme">
+          <span class="slider round"></span>
+        </label>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Drawer",
+    props: {
+      isOpen: Boolean,
+      isDarkTheme: Boolean
+    },
+    methods: {
+        toggleDrawer() {
+          // eslint-disable-next-line
+            console.log('Toggling drawer'); // Check if this logs when the button is clicked
+            this.$emit('toggle-drawer');
+        }
+    },
+
+  }
+  </script>
+  
+  <style scoped>
+  .drawer {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 250px;
+    height: 100%;
+    background-color: var(--background-color);
+    box-shadow: 2px 0 5px rgba(0,0,0,0.5);
+    transform: translateX(-100%);
+    transition: transform 0.3s ease;
+    z-index: 999;
+  }
+  
+  .drawer.is-open {
+    transform: translateX(0);
+  }
+  
+  .switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  transition: .4s;
+  border-radius: 34px;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  transition: .4s;
+  border-radius: 50%;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  transform: translateX(26px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+  </style>
+  
